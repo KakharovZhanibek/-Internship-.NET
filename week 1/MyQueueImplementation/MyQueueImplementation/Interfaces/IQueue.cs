ec96c149efdea672009773fe1a166eb382ef1517
[AkmaralKakhar] <[kahar.akmaral@gmail.com]>
2022-03-28 02:25:29 +0600

    MyQueueImpl

---
+++ week 1/MyQueueImplementation/MyQueueImplementation/Interfaces/IQueue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyQueueImplementation.Interfaces
+{
+    public interface IQueue<T>
+    {   
+        void Enqueue(T item);
+        T Dequeue();
+        T Peek();
+    }
+}
---
+++ week 1/MyQueueImplementation/MyQueueImplementationApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using MyQueueImplementation.Classes;
+using System;
+
+namespace MyQueueImplementationApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyQueue<string> stringMyQueue = new MyQueue<string>();
+
+            MyQueue<int> intMyQueue = new MyQueue<int>();
+
+            stringMyQueue.Enqueue("sretjsertj");
+            stringMyQueue.Enqueue("tdyjtykyuggf");
+
+            intMyQueue.Enqueue(777);
+            intMyQueue.Enqueue(666);
+
+            Console.WriteLine(stringMyQueue.Dequeue());
+            Console.WriteLine(stringMyQueue.Peek());
+
+
+            Console.WriteLine("\n"+intMyQueue.Dequeue());
+            Console.WriteLine(intMyQueue.Peek());
+        }
+    }
+}
