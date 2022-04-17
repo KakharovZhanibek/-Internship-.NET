8846637b5c2f1db3fb9c77390034dd48a7381470
Kakharov.Zhanibek <Kakharov.Zhanibek@outlook.com>
2022-04-17 16:10:39 +0600

    Task Traverse Tree with parallel tasks completed (without tests)

---
+++ week 4/ParallelTreeTraverse/ParallelTreeTraverse/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace ParallelTreeTraverse
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Tree<int> intTree = new Tree<int>() { Data = 39 };
+
+            intTree.Left = new Tree<int>() { Data = 35 };
+            intTree.Right = new Tree<int>() { Data = 44 };
+
+            intTree.Left.Left = new Tree<int>() { Data = 33 };
+            intTree.Left.Right = new Tree<int>() { Data = 37 };
+
+            intTree.Right.Left = new Tree<int>() { Data = 40 };
+            intTree.Right.Right = new Tree<int>() { Data = 47 };
+
+            Action<int> action = x => Console.WriteLine(x);
+
+            DoTree<int>(intTree, action);
+        }
+        public class Tree<T>
+        {
+            public Tree<T> Left;
+            public Tree<T> Right;
+            public T Data;
+        }
+
+        static void DoTree<T>(Tree<T> tree, Action<T> action)
+        {
+            if (tree == null) return;
+            var left = Task.Factory.StartNew(() => DoTree(tree.Left, action));
+            var right = Task.Factory.StartNew(() => DoTree(tree.Right, action));
+            action(tree.Data);
+
+            try
+            {
+                Task.WaitAll(left, right);
+            }
+            catch (AggregateException)
+            {
+
+            }
+        }
+    }
+}
