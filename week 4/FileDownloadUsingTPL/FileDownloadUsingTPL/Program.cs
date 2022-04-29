f3c1cd840b1ab93ba356bf5c19d0e8b10925e170
Kakharov.Zhanibek <Kakharov.Zhanibek@outlook.com>
2022-04-29 22:14:33 +0600

    Added user input url of file

--- week 4/FileDownloadUsingTPL/FileDownloadUsingTPL/Program.cs
+++ week 4/FileDownloadUsingTPL/FileDownloadUsingTPL/Program.cs
@@ -2,10 +2,11 @@
 using FileDownloadUsingTPL;
 
 
-string testUrl = "https://file-examples.com/storage/fee8962188625f1ad9b416a/2017/04/file_example_MP4_1920_18MG.mp4";
-/*"https://file-examples.com/storage/fe55f31641625f0cc982303/2017/10/file_example_JPG_2500kB.jpg"*/
-/*"https://file-examples.com/storage/fe55f31641625f0cc982303/2017/10/file_example_GIF_3500kB.gif";*/
-/*"https://file-examples.com/storage/fe55f31641625f0cc982303/2018/04/file_example_OGG_1920_13_3mg.ogg";*/
+string? testUrl = "";
+Console.WriteLine("Enter download url. You can find some in https://file-examples.com/index.php/sample-video-files/sample-mp4-files/"); ;
+
+testUrl = Console.ReadLine();
+
 
 
 DownloadManager downloadManager = new DownloadManager();
