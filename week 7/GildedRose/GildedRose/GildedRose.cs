6a994e618984289b6c517b863591bed6bd629bd0
Kakharov.Zhanibek <Kakharov.Zhanibek@outlook.com>
2022-05-24 03:52:32 +0600

    GildedRose refactoring completed

---
+++ week 7/GildedRose/GildedRose/GildedRose.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GildedRoseKata
+{
+    public class GildedRose
+    {
+        public const string AGED_BRIE = "Aged Brie";
+        public const string BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+        public const string SULFURAS = "Sulfuras, Hand of Ragnaros";
+        public const string CONJURED = "Conjured";
+        public const int MAXIMUM_QUALITY = 50;
+        public const int MINIMUM_QUALITY = 0;
+        public const int BACKSTAGE_PASSES_THRESHOLD1 = 11;
+        public const int BACKSTAGE_PASSES_THRESHOLD2 = 6;
+
+        IList<Item> Items;
+
+
+        public GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+
+        public void UpdateQuality()
+        {
+            foreach (Item item in Items)
+            {
+                if (IsRegular(item))
+                {
+                    UpdateRegularItem(item);
+                }
+                else if (IsAgedBrie(item))
+                {
+                    UpdateAgedBrieItem(item);
+                }
+                else if (IsBackstagePasses(item))
+                {
+                    UpdateBackStagePassesItem(item);
+                }
+                else if (IsSulfuras(item))
+                {
+                    UpdateSulfurasItem(item);
+                }
+                else if (IsConjured(item))
+                {
+                    UpdateConjuredItem(item);
+                }
+            }
+        }
+
+        private void UpdateConjuredItem(Item item)
+        {
+            item.SellIn--;
+            item.Quality -= 2;
+
+            if (item.SellIn <= 0)
+            {
+                item.Quality -= 2;
+            }
+            if (item.Quality < MINIMUM_QUALITY)
+            {
+                item.Quality = MINIMUM_QUALITY;
+            }
+        }
+
+        private static bool IsConjured(Item item)
+        {
+            return item.Name.StartsWith(CONJURED);
+        }
+
+        private static void UpdateSulfurasItem(Item item)
+        {
+            item.SellIn--;
+        }
+        private static void UpdateBackStagePassesItem(Item item)
+        {
+            item.SellIn--;
+            if (item.SellIn <= 0)
+            {
+                item.Quality = MINIMUM_QUALITY;
+            }
+            else
+            {
+                item.Quality++;
+
+                if (item.SellIn < BACKSTAGE_PASSES_THRESHOLD2)
+                {
+                    item.Quality += 2;
+                }
+                else if (item.SellIn < BACKSTAGE_PASSES_THRESHOLD1)
+                {
+                    item.Quality++;
+                }
+                if (item.Quality > MAXIMUM_QUALITY)
+                {
+                    item.Quality = MAXIMUM_QUALITY;
+                }
+            }
+        }
+
+        private static void UpdateAgedBrieItem(Item item)
+        {
+            item.SellIn--;
+            item.Quality++;
+
+            if (item.SellIn <= 0)
+            {
+                item.Quality++;
+            }
+
+            if (item.Quality > MAXIMUM_QUALITY)
+            {
+                item.Quality = MAXIMUM_QUALITY;
+            }
+        }
+
+        private static void UpdateRegularItem(Item item)
+        {
+            item.SellIn--;
+            item.Quality--;
+
+            if (item.SellIn <= 0)
+            {
+                item.Quality--;
+            }
+            if (item.Quality < 0)
+            {
+                item.Quality = 0;
+            }
+        }
+
+        private static bool IsRegular(Item item)
+        {
+            return !(IsAgedBrie(item) || IsBackstagePasses(item) || IsSulfuras(item) || IsConjured(item));
+        }
+        private static bool IsSulfuras(Item item)
+        {
+            return item.Name == SULFURAS;
+        }
+
+        private static bool IsBackstagePasses(Item item)
+        {
+            return item.Name == BACKSTAGE_PASSES;
+        }
+
+        private static bool IsAgedBrie(Item item)
+        {
+            return item.Name == AGED_BRIE;
+        }
+    }
+}
---
+++ week 7/GildedRose/GildedRoseTests/GildedRoseTest.cs
@@ -0,0 +1,112 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using GildedRoseKata;
+
+namespace GildedRoseTests
+{
+    public class GildedRoseTest
+    {
+        public Item CreateAndUpdateItem(string name, int sellIn, int quality)
+        {
+            IList<Item> items = new List<Item> { new Item { Name = name, SellIn = sellIn, Quality = quality } };
+            GildedRose app = new GildedRose(items) ;
+
+            app.UpdateQuality();
+
+            return items[0];
+        }
+
+        [Fact]
+        public void GildedRose_GivenRegularItem_QualityAndSellInDecreases()
+        {
+            Item item = CreateAndUpdateItem("TestItem", 13, 7);
+
+            Assert.Equal(6, item.Quality);
+            Assert.Equal(12, item.SellIn);
+        }
+        [Fact]
+        public void GildedRose_GivenRegularItemWithExpiredSellIn_QualityDecreasesTwiceAsFast()
+        {
+            Item item = CreateAndUpdateItem("TestItem", 0, 31);
+
+            Assert.Equal(29, item.Quality);
+        }
+        [Fact]
+        public void GildedRose_GivenRegularItem_QualityCanNotBeNegative()
+        {
+            Item item = CreateAndUpdateItem("TestItem", 10, 0);
+
+            Assert.Equal(0, item.Quality);
+        }
+        [Fact]
+        public void GildedRose_GivenAgedBrie_QualityIncreases()
+        {
+            Item item = CreateAndUpdateItem(GildedRose.AGED_BRIE, 10, 31);
+
+            Assert.Equal(32, item.Quality);
+        }
+        [Fact]
+        public void GildedRose_GivenAgedBrie_QualityCanNotBeMoreThanMaximumQuality()
+        {
+            Item item = CreateAndUpdateItem(GildedRose.AGED_BRIE, 10, 50);
+
+            Assert.Equal(GildedRose.MAXIMUM_QUALITY, item.Quality);
+        }
+        [Fact]
+        public void GildedRose_GivenSulfuras_QualityNeverDegrades()
+        {
+            Item item = CreateAndUpdateItem(GildedRose.SULFURAS, 0, 80);
+
+            Assert.Equal(80, item.Quality);
+        }
+        [Fact]
+        public void GildedRose_GivenBackstagePasses_QualityIncreases()
+        {
+            Item item = CreateAndUpdateItem(GildedRose.BACKSTAGE_PASSES, 26, 10);
+
+            Assert.Equal(11, item.Quality);
+        }
+        [Fact]
+        public void GildedRose_GivenBackstagePasses10DaysOut_QualityIncreasesByBackstagePassesThreshold1()
+        {
+            Item item = CreateAndUpdateItem(GildedRose.BACKSTAGE_PASSES, 9, 10);
+
+            Assert.Equal(12, item.Quality);
+        }
+        [Fact]
+        public void GildedRose_GivenBackstagePasses5DaysOut_QualityIncreasesByBackstagePassesThreshold2()
+        {
+            Item item = CreateAndUpdateItem(GildedRose.BACKSTAGE_PASSES, 4, 10);
+
+            Assert.Equal(13, item.Quality);
+        }
+        [Fact]
+        public void GildedRose_GivenBackstagePassesWithExpiredSellIn_QualityDecreasesToMinimumQuality()
+        {
+            Item item = CreateAndUpdateItem(GildedRose.BACKSTAGE_PASSES, 0, 10);
+
+            Assert.Equal(GildedRose.MINIMUM_QUALITY, item.Quality);
+        }
+        [Fact]
+        public void GildedRose_GivenConjuredItem_QualityDecreasesTwiceAsFast()
+        {
+            Item item = CreateAndUpdateItem(GildedRose.CONJURED, 20, 10);
+
+            Assert.Equal(8, item.Quality);
+        }
+        [Fact]
+        public void GildedRose_GivenConjuredItemWithExpiredSellIn_QualityDecreasesTwiceAsFastThanUsual()
+        {
+            Item item = CreateAndUpdateItem(GildedRose.CONJURED, -2, 10);
+
+            Assert.Equal(6, item.Quality);
+        }
+        [Fact]
+        public void GildedRose_GivenConjuredItem_QualityCanNotBeNegative()
+        {
+            Item item = CreateAndUpdateItem("TestItem", 10, 0);
+
+            Assert.Equal(GildedRose.MINIMUM_QUALITY, item.Quality);
+        }
+    }
+}
